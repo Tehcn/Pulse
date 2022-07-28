f35688c08488a95a6a6a5e450d98f070b3bc1c75
Noah Perry <noah@perrybrain.com>
2022-07-28 16:53:51 -0500

    Initial commit

---
+++ src/main/java/dev/tehcn/pulse/Pulse.java
@@ -0,0 +1,13 @@
+package dev.tehcn.pulse;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Pulse implements ModInitializer {
+	public static final String MOD_ID = "pulse";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() { }
+}
---
+++ src/main/java/dev/tehcn/pulse/PulseClient.java
@@ -0,0 +1,11 @@
+package dev.tehcn.pulse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PulseClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
